54cd5c2746b8dc36f965e3e69a774609d7d9c85c
Guilherme <guitoledo13@hotmail.com>
2026-04-06 22:08:29 -0300

    Att 1.0.3.1 - Adding some tests

--- backend/posts/tests/test_post.py
+++ backend/posts/tests/test_post.py
@@ -2,8 +2,14 @@ import pytest
 from django.core.exceptions import ValidationError
 from users.factories.user_factory import UserFactory
 from posts.factories.post_factory import PostFactory
-from posts.serializers import PostSerializer
 from rest_framework.test import APIClient
+
+def test_dont_allow_a_1200_post(db):
+    long_post = "o" * 1201
+    post = PostFactory.build(post_body=long_post)
+
+    with pytest.raises(ValidationError):
+        post.full_clean()
 
 def test_like_post(db):
     user1 = UserFactory()
@@ -40,3 +46,14 @@ def test_comment_in_a_post(db):
     
     assert response.status_code == 200
     assert response.data["status"] == "commented"
+
+def test_dont_allow_comment_exced_max(db):
+    user1 = UserFactory()
+    post = UserFactory()
+
+    client = APIClient()
+    client.force_authenticate(user=user1)
+
+    response = client.post(f"/api/posts/{post.pk}/comment/",{ "body": "0"*501}, format="json")
+
+    assert response.erros
