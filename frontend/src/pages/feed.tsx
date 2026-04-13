81ef24ab9ef04eb311b0396276e5d294b3272f5f
Guilherme <guitoledo13@hotmail.com>
2026-04-13 11:56:33 -0300

    Att 2.0.5 - Finishing Posts in feed

--- frontend/src/pages/feed.tsx
+++ frontend/src/pages/feed.tsx
@@ -1,10 +1,13 @@
+import { useEffect, useState } from "react";
 import { Post } from "../components/post";
+import axios from "axios";
 
 type PostProps = {
+    id: number;
   name: string;
   username: string;
   profileImage: string;
-  content: strinsg;
+  content: string;
   postImage?: string;
   comments: number;
   likes: number;
@@ -12,12 +15,25 @@ type PostProps = {
   created_at: string;
 };
 
-
-
 export function Feed() {
+    const [Posts, UsePosts] = useState<PostProps[]>([])
+    
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const response = await axios.get("http://127.0.0.1:8000/api/posts/");
+                UsePosts(response.data.results)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchPosts()
+    }, [])
     return (
         <div>
-            <Post />
+            {Posts.map((post) => (
+                <Post key={post.id} name={post.name} username={post.username} profileImage={post.profileImage} comments={post.comments} likes={post.likes} retweets={post.retweets} content={post.content} postImage={post.postImage} created_at={post.created_at} />
+            ))}
         </div>
     )
 }
