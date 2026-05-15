e6efc77d35b75ef5c468620cd735d71680d46fba
Guilherme <guilhermemonteirotoledo@gmail.com>
2026-05-15 17:12:27 -0300

    Att 3.2.6 - Continuing retweet post

--- frontend/src/pages/feed.tsx
+++ frontend/src/pages/feed.tsx
@@ -382,16 +382,16 @@ export function Feed() {
               post, // HERE HERE HERE HERE HERE HERE HERE HERE
             ) => {
               const isLiked = actualUser
-                ? post.likes.includes(actualUser.id)
+                ? post.retweet_post?.likes.includes(actualUser.id)
                 : false;
               const isRetweeted = actualUser
-                ? post.retweets.includes(actualUser.id)
+                ? post.retweet_post?.retweets.includes(actualUser.id)
                 : false;
               
-              if (post.parent_post === null) {
+              if (post.parent_post === null && post.retweet_post === null) {
                 return (
                   <div
-                    className="bg-black flex pr-8 pb-2 pt-2 pl-2 mr-2 border-b border-stone-800 w-[100%] cursor-pointer relative"
+                    className="bg-black flex pr-8 pb-4 pt-4 pl-2 mr-2 border-b border-stone-800 w-[100%] cursor-pointer relative"
                     key={post.id}
                     onClick={() => navigate(`/post/${post.id}`)}
                   >
@@ -514,6 +514,146 @@ export function Feed() {
                       </div>
                     </div>
                   </div>
+                );
+              }
+              else if (post.retweet_post !== null) {
+                console.log(post)
+                return (
+            <div
+                className="bg-black flex pr-8 pb-4 pt-4 pl-2 mr-2 border-b border-stone-800 w-[100%] cursor-pointer relative"
+                key={post.id}
+                onClick={() => navigate(`/post/${post.id}`)}
+            >
+                {actualUser?.id === post.author.id && (
+                    <button
+                        className="font-white absolute h-8 w-8 flex items-center justify-center top-3 right-3 cursor-pointer hover:bg-stone-700 p-2 rounded-full transition-colors duration-300"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            openClosePostMenu(post.id);
+                        }}
+                    >
+                        •••
+                    </button>
+                )}
+
+                {openedPostMenu === post.id && (
+                    <div className="absolute top-12 right-3 w-56 bg-black border border-stone-800 rounded-2xl shadow-xl z-50 transition-colors duration-300">
+                        <h1
+                            className="text-red-500 font-bold ml-4"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                deletePost(post.id);
+                            }}
+                        >
+                            Delete post
+                        </h1>
+                    </div>
+                )}
+
+                <img
+                    className="rounded-full w-[48px] h-[48px] cursor-pointer self-start"
+                    src={post.author.profile_image}
+                    alt="profile_picture"
+                />
+
+                <div className="flex flex-col ml-3 w-full">
+                    <div className="flex items-center">
+                        <h2 className="pr-1 text-[#E7E9EA] text-[16px] cursor-pointer">
+                            {post.author.name}
+                        </h2>
+
+                        <h2 className="pr-1 text-stone-500 text-[16px]">
+                            @{post.author.username}
+                        </h2>
+
+                        <h4 className="text-stone-500 text-[16px]">
+                            · {CalcTemp(post.created_at)}
+                        </h4>
+                    </div>
+
+                    <h2 className="text-[#E7E9EA] text-[18px]">
+                        {post.post_body}
+                    </h2>
+
+                    {post.medias &&
+                        post.medias.map((media) => (
+                            <img
+                                className="w-full rounded-md block mt-4 mb-4 object-cover cursor-pointer"
+                                src={media.file}
+                                alt=""
+                                key={media.id}
+                            />
+                        ))}
+
+                    <div className="flex justify-center gap-32 mt-4">
+                        <div
+                            className="flex items-center group cursor-pointer"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                setSelectedPost(post);
+                            }}
+                        >
+                            <CommentIcon className="fill-stone-500 cursor-pointer group-hover:fill-blue-500 w-6 h-6 transition-colors duration-300" />
+
+                            <h2 className="text-stone-500 ml-1 group-hover:text-blue-500 transition-colors duration-300">
+                                {post.comments.length}
+                            </h2>
+                        </div>
+
+                        <div
+                            className="flex items-center group cursor-pointer"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                retweet(post.id);
+                            }}
+                        >
+                            <RetweetIcon
+                                className={`w-6 h-6 transition-colors duration-300 ${
+                                    isRetweeted
+                                        ? "fill-green-500"
+                                        : "fill-stone-500 group-hover:fill-green-500"
+                                }`}
+                            />
+
+                            <h2
+                                className={`ml-1 transition-colors duration-300 ${
+                                    isRetweeted
+                                        ? "text-green-500"
+                                        : "text-stone-500"
+                                }`}
+                            >
+                                {post.retweets.length}
+                            </h2>
+                        </div>
+
+                        <div
+                            className="flex items-center group cursor-pointer"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                like(post.id);
+                            }}
+                        >
+                            <LikeIcon
+                                className={`w-6 h-6 transition-colors duration-300 ${
+                                    isLiked
+                                        ? "fill-red-600"
+                                        : "fill-stone-500 group-hover:fill-red-600"
+                                }`}
+                            />
+
+                            <h2
+                                className={`ml-1 transition-colors duration-300 ${
+                                    isLiked
+                                        ? "text-red-600"
+                                        : "text-stone-500"
+                                }`}
+                            >
+                                {post.likes.length}
+                            </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
                 );
               }
             },
