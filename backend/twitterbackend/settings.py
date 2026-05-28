ce56bd55942c7a3ba3b205bd619cdd6467f7b1fb
Guilherme <guilhermemonteirotoledo@gmail.com>
2026-05-28 17:47:34 -0300

    Att 1.0-2 - Fixing tests

--- backend/twitterbackend/settings.py
+++ backend/twitterbackend/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "twitterbackend.wsgi.application"
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
     )
 }
 
